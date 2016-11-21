c17dd88a74342e54c520a1c29116b8c65a9463ad
vrunoa <vruno.alassia@gmail.com>
2016-11-20 21:12:22 -0300

    fill workshop list

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,8 @@
 var controllers = angular.module('tailerControllers', [])
 controllers.controller('EditorController', ['$scope', function ($scope) {
   $scope.list = w.getWorkshops()
+  $scope.selected_workshop = false
+  $scope.selectWorkshop = (name) => {
+    console.log($scope.selected_workshop)
+  }
 }])
